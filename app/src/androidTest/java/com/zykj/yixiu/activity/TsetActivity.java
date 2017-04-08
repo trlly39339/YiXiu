f5a352374ca6be6e232731a62be1d427db83aa35
trlly39339 <393398296@qq.com>
2017-04-08 13:48:48 +0800

    创建了一个测试activity与activity.xml

---
+++ app/src/androidTest/java/com/zykj/yixiu/activity/TsetActivity.java
@@ -0,0 +1,19 @@
+package com.zykj.yixiu.activity;
+
+import android.os.Bundle;
+
+import com.zykj.yixiu.R;
+import com.zykj.yixiu.base.BaseActivity;
+
+/**
+ * Created by zykj on 2017/4/8.
+ */
+
+public class TsetActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.base_acyivity);
+    }
+}
---
+++ app/src/androidTest/java/com/zykj/yixiu/base/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.zykj.yixiu.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zykj on 2017/4/8.
+ */
+
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
