69ca110d5392eab0336722f6c5ddb00a8db94fa3
trlly39339 <393398296@qq.com>
2017-04-10 14:13:45 +0800

    添加了友盟基本集成准备工作

--- app/src/main/java/com/zykj/yixiu/app/activity/app/App.java
+++ app/src/main/java/com/zykj/yixiu/app/activity/app/App.java
@@ -2,9 +2,21 @@ package com.zykj.yixiu.app.activity.app;
 
 import android.app.Application;
 
+import com.umeng.analytics.MobclickAgent;
+
 /**
  * Created by zykj on 2017/4/8.
  */
 
 public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL);
+
+    }
+
+
+
 }
--- app/src/main/java/com/zykj/yixiu/app/activity/base/BaseActivity.java
+++ app/src/main/java/com/zykj/yixiu/app/activity/base/BaseActivity.java
@@ -3,6 +3,8 @@ package com.zykj.yixiu.app.activity.base;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.umeng.analytics.MobclickAgent;
+
 /**
  * Created by zykj on 2017/4/8.
  */
@@ -12,5 +14,13 @@ public class BaseActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
     }
 }
